a6cef4bb4025fb14cdcdd2602171ecd3f7b235c6
CanMage <160321121+CanMageOfficial@users.noreply.github.com>
2025-03-16 16:36:11 -0700

    Fixed the bug deploying using profile

--- 2-deploy.sh
+++ 2-deploy.sh
@@ -3,12 +3,12 @@ set -eo pipefail
 ARTIFACT_BUCKET=$(cat bucket-name.txt)
 TEMPLATE=template.yml
 
-read -p 'Email Address: ' emailAddress
+read -p 'Email Address To Receive Daily Report: ' emailAddress
 read -p "AWS Profile Name, leave empty for default value: " awsProfile
 awsProfile=${awsProfile:-default}
 
 ./gradlew buildZip
-aws cloudformation package --template-file $TEMPLATE --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
+aws cloudformation package --template-file $TEMPLATE --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml --profile=$awsProfile
 aws cloudformation deploy --template-file out.yml --stack-name cloudsavvy \
     --parameter-overrides EmailAddress=$emailAddress LambdaBucket=$ARTIFACT_BUCKET \
     --capabilities CAPABILITY_NAMED_IAM --profile=$awsProfile
